2369cc90a6a2429972c6f242467d5d34010e1e9c
Vijay <vshukla684@gmail.com>
2019-02-07 19:01:17 +0530

    Use Real Example with AUTH

--- portal/db-service/src/main/java/com/portal/DbServiceApplication.java
+++ portal/db-service/src/main/java/com/portal/DbServiceApplication.java
@@ -3,13 +3,19 @@ package com.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.portal.repository")
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+@EnableMongoAuditing
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class DbServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DbServiceApplication.class, args);
